6c6a7468fbbf025564aec571e3eda55feb312d2f
CHIGODI <chiegody254@gmail.com>
2024-04-19 15:25:28 +0300

    Remove empty lines

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,7 +6,7 @@ from markupsafe import escape
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from models import storage
-from  models.state import State
+from models.state import State
 
 
 app = Flask(__name__)
