4a2e2ff9040dcaa5737935a8f66633740eab4af2
CHIGODI <chiegody254@gmail.com>
2024-04-19 15:12:54 +0300

    Add task 7

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -58,4 +58,4 @@ def number_odd_or_even(n):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=1)
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This module conatins a simple Flask App """
+
+from flask import Flask, render_template
+from markupsafe import escape
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from models import storage
+from  models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ """
+    states = storage.all(State)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def delete_session(exception=None):
+    """"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
