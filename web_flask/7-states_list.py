8ad6b8ceddbe7acdba6290893f8ea9f6d1c54820
CHIGODI <chiegody254@gmail.com>
2024-04-19 21:34:47 +0300

    £

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,13 +3,16 @@
 
 from flask import Flask, render_template
 from markupsafe import escape
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def delete_session(exception=None):
+    """ ends a session/ deletes it to create a new one"""
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -18,12 +21,6 @@ def states_list():
     states = storage.all(State)
     sorted_states = sorted(states.values(), key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
-
-
-@app.teardown_appcontext
-def delete_session(exception=None):
-    """ ends a  session/ deletes it to create a new one"""
-    storage.close()
 
 
 if __name__ == '__main__':
